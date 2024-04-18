840f99f8b64dbed2ea6b0a9e93e6051e2fbc3bb4
bruce.lu <bruce.lu@bridgewell.com>
2024-04-18 09:57:36 +0000

    feat: initial sign-up page

--- ecomprj/ecomprj/urls.py
+++ ecomprj/ecomprj/urls.py
@@ -21,7 +21,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("core.urls")),
+    path("user/", include("userauths.urls")),
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
