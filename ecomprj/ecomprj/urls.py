40d29330aeca810c6f2e5505d2bb2cfb39467fcf
bruce.lu <bruce.lu@bridgewell.com>
2024-04-18 07:44:21 +0000

    feat: create custom auth User model

--- ecomprj/ecomprj/urls.py
+++ ecomprj/ecomprj/urls.py
@@ -21,10 +21,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    # path("admin/", admin.site.urls),
-    path("", include("core.urls"))
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("core.urls"))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
