bcb47e7dc915ab2e0e152749827e3b33a580611c
bruce.lu <bruce.lu@bridgewell.com>
2024-04-19 03:48:32 +0000

    feat: create sign-up function

--- ecomprj/userauths/views.py
+++ ecomprj/userauths/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from userauths.forms import UserRegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 
 def register_view(request):
-    return render(request, "userauths/sign-up.html")
+
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hey {username}, your account has been created!")
+            # new_user = authenticate(
+            #     username=form.cleaned_data["email"],
+            #     password=form.cleaned_data["password1"],
+            # )
+            login(request, new_user)
+            return redirect("core:index")
+    else:
+        form = UserRegisterForm()
+
+    context = {"form": form}
+    return render(request, "userauths/sign-up.html", context)
