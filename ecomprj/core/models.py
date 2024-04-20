1cd2e9afb33b39d2b480280764c9bcacdee94406
bruce.lu <bruce.lu@bridgewell.com>
2024-04-20 03:33:00 +0000

    feat: model Vendor

--- ecomprj/core/models.py
+++ ecomprj/core/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from shortuuid.django_fields import ShortUUIDField
 from django.utils.html import mark_safe
+from userauths.models import User
+
+
+def user_directory_path(instance, filename):
+    return f"user_{instance.user.id}/{filename}"
 
 
 class Category(models.Model):
@@ -20,3 +25,22 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Vendor(models.Model):
+    vid = ShortUUIDField(
+        unique=True, length=10, max_length=20, prefix="ven", alphabet="abcdefgh12345"
+    )
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="user_directory_path")
+    description = models.TextField(null=True, blank=True)
+
+    address = models.CharField(max_length=100, default="N/A")
+    contact = models.CharField(max_length=100, default="N/A")
+    chat_resp_time = models.CharField(max_length=100, default="N/A")
+    shipping_on_time = models.CharField(max_length=100, default="N/A")
+    authentic_rating = models.CharField(max_length=100, default="N/A")
+    days_return = models.CharField(max_length=100, default="N/A")
+    warranty_period = models.CharField(max_length=100, default="N/A")
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
