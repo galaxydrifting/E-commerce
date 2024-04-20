5b078b0fb5badea1f9ad3da679e51fb548e998d9
bruce.lu <bruce.lu@bridgewell.com>
2024-04-20 06:39:00 +0000

    feat: admin.site.register

--- ecomprj/core/models.py
+++ ecomprj/core/models.py
@@ -152,7 +152,7 @@ class CartOrder(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     paid_status = models.BooleanField(default=False)
-    date = models.DateTimeField(auto_now_add=True)
+    order_date = models.DateTimeField(auto_now_add=True)
     product_status = models.CharField(
         choices=STATUS_CHOICE, max_length=30, default="processing"
     )
@@ -163,6 +163,7 @@ class CartOrder(models.Model):
 
 class CartOrderItems(models.Model):
     order = models.ForeignKey(CartOrder, on_delete=models.CASCADE)
+    invoice_no = models.CharField(max_length=200)
     product_status = models.CharField(max_length=200)
     item = models.CharField(max_length=200)
     image = models.CharField(max_length=200)
