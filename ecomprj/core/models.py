7c54ba443ecd2a04239805cc55b235d99f29f231
bruce.lu <bruce.lu@bridgewell.com>
2024-04-20 03:13:31 +0000

    feat: model Category

--- ecomprj/core/models.py
+++ ecomprj/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+from django.utils.html import mark_safe
 
-# Create your models here.
+
+class Category(models.Model):
+    cid = ShortUUIDField(
+        unique=True, length=10, max_length=20, prefix="cat", alphabet="abcdefgh12345"
+    )
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="category")
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def category_image(self):
+        # fmt: off
+        return mark_safe(f'<img src="{self.image.url}" width="50" height="50" />')
+        # fmt: on
+
+    def __str__(self):
+        return self.title
