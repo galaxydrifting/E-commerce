0053de57e6deca58b1c0a642f136f4cc562216af
bruce.lu <bruce.lu@bridgewell.com>
2024-04-20 08:59:20 +0000

    feat: list product from database

--- ecomprj/core/views.py
+++ ecomprj/core/views.py
@@ -1,6 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from core.models import (
+    Product,
+    Category,
+    Vendor,
+    CartOrder,
+    CartOrderItems,
+    ProductImages,
+    ProductReview,
+    Wishlist,
+    Address,
+)
 
 
 def index(request):
-    return render(request, "core/index.html")
+    products = Product.objects.all().order_by("-id")
+    context = {"products": products}
+    return render(request, "core/index.html", context)
