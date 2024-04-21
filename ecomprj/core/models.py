6596e415ef068ef7820cc9a5c39bd91c6bdcce45
bruce.lu <bruce.lu@bridgewell.com>
2024-04-21 13:33:37 +0000

    chore: before migrate

--- ecomprj/core/models.py
+++ ecomprj/core/models.py
@@ -60,6 +60,9 @@ class Vendor(models.Model):
     )
     title = models.CharField(max_length=100, default="N/A")
     image = models.ImageField(upload_to="user_directory_path", default="vendor.jpg")
+    cover_image = models.ImageField(
+        upload_to="user_directory_path", default="vendor.jpg"
+    )
     description = models.TextField(null=True, blank=True, default="N/A")
 
     address = models.CharField(max_length=100, default="N/A")
