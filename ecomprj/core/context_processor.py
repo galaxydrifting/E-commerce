38fa2ad2537db4cdeff55dfa02e514210006112b
bruce.lu <bruce.lu@bridgewell.com>
2024-04-25 14:55:19 +0000

    feat: in case admin error

--- ecomprj/core/context_processor.py
+++ ecomprj/core/context_processor.py
@@ -13,5 +13,8 @@ from core.models import (
 
 def default(request):
     categories = Category.objects.all()
-    address = Address.objects.get(user=request.user)
+    try:
+        address = Address.objects.get(user=request.user)
+    except Exception:
+        address = None
     return {"categories": categories, "address": address}
